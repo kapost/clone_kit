58af8625f6388a6d3a727986877a37d51f524966
ebrohman <ericbrohman@gmail.com>
2018-04-19 13:03:45 -0600

    add clone spec to example doc

--- spec/support/example_doc.rb
+++ spec/support/example_doc.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
+require File.expand_path(
+          "../../lib/clone_kit/cloners/mongoid_ruleset_cloner",
+          File.dirname(__FILE__))
+
+require File.expand_path(
+          "../../lib/clone_kit/emitters/base_mongoid_emitter",
+          File.dirname(__FILE__))
 
 class EmbeddedExampleDoc
   include Mongoid::Document
@@ -27,4 +34,9 @@ class ExampleDoc
   embeds_one :another_embedded_example_doc
 
   validates :name, presence: true
+
+  CloneKit::Specification.new(self) do |spec|
+    spec.cloner = CloneKit::Cloners::MongoidRulesetCloner.new(self)
+    spec.emitter = CloneKit::Emitters::BaseMongoidEmitter.new(self)
+  end
 end
