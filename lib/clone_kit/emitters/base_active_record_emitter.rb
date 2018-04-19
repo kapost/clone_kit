69728bb84bf38871f31d4d2845f406648591615f
ebrohman <ericbrohman@gmail.com>
2018-04-19 13:02:24 -0600

    Added ActiveRecord emitter

---
+++ lib/clone_kit/emitters/base_active_record_emitter.rb
@@ -0,0 +1,22 @@
+module CloneKit
+  module Emitters
+    class BaseActiveRecordEmitter
+      def initialize(model_klass, query: :all)
+        self.klass = model_klass
+        self.query = query
+      end
+
+      def scope(args)
+        klass.send(:query)
+      end
+
+      def emit_all
+        scope
+      end
+
+      private
+
+      attr_accessor :klass, :query
+    end
+  end
+end
