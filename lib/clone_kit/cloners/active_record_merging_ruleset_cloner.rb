a8396987e8cfdf243d3e4f2b997df18abc62dcfc
ebrohman <ericbrohman@gmail.com>
2018-04-23 16:43:20 -0600

    spec active_record_merging_ruleset_cloner
    - remove call to clone embedded fields

--- lib/clone_kit/cloners/active_record_merging_ruleset_cloner.rb
+++ lib/clone_kit/cloners/active_record_merging_ruleset_cloner.rb
@@ -5,6 +5,8 @@ require "clone_kit/cloners/active_record_ruleset_cloner"
 module CloneKit
   module Cloners
     class ActiveRecordMergingRulesetCloner < ActiveRecordRulesetCloner
+      ID = "id".freeze
+
       def initialize(model_klass, rules: [], merge_fields: ["name"])
         super(model_klass, rules: rules)
         self.merge_fields = merge_fields
@@ -27,7 +29,6 @@ module CloneKit
       def merge(records)
         result = nil
         records.each do |rec|
-          clone_all_embedded_fields(rec)
           result = if result.nil?
                      rec.deep_dup
                    else
@@ -42,59 +43,59 @@ module CloneKit
 
       attr_accessor :merge_fields
 
-      def find_and_merge_existing_records(ids)
+      def find_and_merge_existing_records(ids, mergeable: Set.new, skip: skip = Set.new)
         all_records = []
         each_existing_record(ids) do |rec|
           all_records << rec
         end
 
         result = []
-        skip = Set.new
 
         all_records.each_with_index do |record, i|
-          next if skip.include?(record[:id])
-          mergeable = [record]
+          next if skip.include?(record[ID])
+          mergeable << record
 
           all_records[i + 1..all_records.length].each do |other_record|
             next unless compare(record, other_record)
 
             mergeable << other_record
-            skip << other_record[:id]
+            skip << other_record[ID]
           end
 
           new_id = generate_new_id
           new_record = if mergeable.length == 1
-                         copy = clone(mergeable[0])
-                         @saved_id_map[copy[:id]] = new_id
-                         copy
+                          copy = clone(mergeable.first)
+                          @saved_id_map[copy[ID]] = new_id
+                          copy
                        else
-                         merged = merge(mergeable)
+                          merged = merge(mergeable)
 
-                         mergeable.each do |m|
-                           @saved_id_map[m[:id]] = new_id
-                         end
-                         merged
+                          mergeable.each do |m|
+                            @saved_id_map[m[ID]] ||= new_id
+                          end
+                          merged
                        end
 
-          new_record[:id] = new_id
+          new_record[ID] = new_id
+
           result << new_record
         end
 
         CloneKit::SharedIdMap
           .new(current_operation.id)
-          .insert_many(model_klass, @saved_id_map, id_generator: id_generator)
+          .insert_many(model_klass, @saved_id_map)
 
         result
       end
 
       def apply_rules_and_save(records)
         records.each do |attributes|
-          id = attributes[:id]
+          id = attributes[ID]
           rules.each do |rule|
             begin
               rule.fix(@saved_id_map.key(id), attributes)
             rescue StandardError => e
-              id = attributes[:id]
+              id = attributes[ID]
               message = "Unhandled error when applying rule #{rule.class.name} to #{model_klass} #{id}: #{e.class}"
               current_operation.error(message)
             end
