05cb26987b2e302d2e7b98e15fa739144d444cea
ebrohman <ericbrohman@gmail.com>
2018-04-23 14:39:16 -0600

    remove #configure & id_generator setting from Specification

--- lib/clone_kit/active_record_specification.rb
+++ lib/clone_kit/active_record_specification.rb
@@ -5,13 +5,7 @@ require "clone_kit/id_generators/uuid"
 
 module CloneKit
   class ActiveRecordSpecification < Specification
-    attr_writer :id_generator
-
     protected
-
-    def configure
-      self.id_generator = IdGenerators::Uuid.new
-    end
 
     def validate!
       fail SpecificationError, "Model type not supported" unless active_record_document?
--- lib/clone_kit/mongo_specification.rb
+++ lib/clone_kit/mongo_specification.rb
@@ -5,13 +5,7 @@ require "clone_kit/id_generators/bson"
 
 module CloneKit
   class MongoSpecification < Specification
-    attr_writer :id_generator
-
     protected
-
-    def configure
-      self.id_generator = IdGenerators::Bson
-    end
 
     def validate!
       fail SpecificationError, "Model type not supported" unless mongoid_document?
