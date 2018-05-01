96aaf318205e4c76857ec6d4c82d9fb97ba96a02
Matt Huggins <matt.huggins@gmail.com>
2018-05-01 17:23:40 -0500

    Fixed with_connection placement

--- lib/clone_kit/cloners/active_record_ruleset_cloner.rb
+++ lib/clone_kit/cloners/active_record_ruleset_cloner.rb
@@ -80,19 +80,20 @@ module CloneKit
           )
         end
 
-        with_connection(current_operation.arguments["write_database_url"]) do
+        with_connection(current_operation.arguments["write_postgres_url"]) do
           ActiveRecord::Base.connection.execute(insert_op.to_sql)
         end
       end
 
       def each_existing_record(ids)
-        with_connection(current_operation.arguments["read_database_url"]) do
-          ids.each do |id|
-            record = model_klass.find_by(model_klass.primary_key => id).attributes
-            next if record.nil?
-
-            yield record
+        ids.each do |id|
+          record = with_connection(current_operation.arguments["read_postgres_url"]) do
+            model_klass.find_by(model_klass.primary_key => id).attributes
           end
+
+          next if record.nil?
+
+          yield record
         end
       end
 
