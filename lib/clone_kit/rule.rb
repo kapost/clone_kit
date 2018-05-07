7c6ac58ea650b7ead4f3983ed1f101b6eeb22970
Adam Steel <adam.steel@kapost.com>
2018-05-07 12:17:10 -0600

    [refactor] pass id map class in for easier testing

--- lib/clone_kit/rule.rb
+++ lib/clone_kit/rule.rb
@@ -4,8 +4,9 @@ module CloneKit
   class Rule
     attr_accessor :id_generator
 
-    def initialize(id_generator: nil)
+    def initialize(id_generator: nil, id_map_class: CloneKit::SharedIdMap)
       @id_generator = id_generator
+      @id_map_class = id_map_class
     end
 
     def current_operation=(operation)
@@ -15,14 +16,14 @@ module CloneKit
 
     protected
 
-    attr_reader :current_operation
+    attr_reader :current_operation, :id_map_class
 
     def operation_arguments
       current_operation.arguments
     end
 
     def shared_id_map
-      @shared_id_map ||= CloneKit::SharedIdMap.new(current_operation.id)
+      @shared_id_map ||= id_map_class.new(current_operation.id)
     end
 
     def warn_event(message)
